3f78c41286292b62e7bb9326f05b52ea78ad5011
Eivind Fonn <evfonn@gmail.com>
2021-06-04 10:56:45 +0200

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Grevling',
-    version='0.1.0',
+    version='1.0.0',
     description='A batch runner tool',
     author='Eivind Fonn',
     author_email='eivind.fonn@sintef.no',
