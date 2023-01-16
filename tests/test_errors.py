98f741ef2bd87c67855ef9d14800740cf3523c64
Eivind Fonn <evfonn@gmail.com>
2023-01-16 13:47:27 +0100

    Fix error tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from pytest import raises, mark
-from jsonschema.exceptions import ValidationError
+from pydantic import ValidationError
 
 from grevling import Case
 
@@ -28,6 +28,5 @@ ERRORFILES = [
     'filename', [DATADIR / 'erroring' / f'{fn}.yaml' for fn in ERRORFILES]
 )
 def test_raises(filename):
-    # with raises(ValidationError):
-    #     Case(filename)
-    pass
+    with raises(ValidationError):
+        Case(filename)
