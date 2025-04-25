29ad84362501ad06f99457743594bfd31f34cec8
Eivind Fonn <evfonn@gmail.com>
2025-04-25 13:05:15 +0200

    Bump version to 2.1.0

--- grevling/__init__.py
+++ grevling/__init__.py
@@ -25,7 +25,7 @@ from .workflow.local import LocalWorkspaceCollection, LocalWorkspace, LocalWorkf
 from . import util, api
 
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 
 class CaseState(PersistentObject):
